fd9f315dc8263ef19877ded190e68fed408f49bc
cb0n3y <cb0n3y@gmail.com>
2022-01-10 08:51:44 +0100

    delete commented line in setruby.pp

--- manifests/setruby.pp
+++ manifests/setruby.pp
@@ -7,14 +7,11 @@
 class mailcatcher::setruby {
   package { 'ruby':
     ensure       => '2.7',
-    # name     => $mailcatcher::ruby_version,
     provider     => $mailcatcher::module_mngmt,
-    # enable_only  => true,
   }
   package { $mailcatcher::packages:
     ensure => present,
   }
 
-  # Package['enable ruby module'] -> Package['ruby-devel']
   Package['ruby'] -> Package['ruby-devel']
 }
