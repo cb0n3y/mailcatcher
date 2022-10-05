dc718d26ceef690597cacb51e60eec8e58c01576
Angel Ortiz <angelfranklin.ortizsuarez@digitaspixelpark.com>
2022-10-05 14:35:03 +0200

    add reviewed code

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,32 @@
-# @summary A short summary of the purpose of this class
+# @summary A class for mailcatching
 #
-# A description of what this class does
+# The software catches mail before are they send out and redirect to a UI.
+# These mails will never leave the host.
 #
-# @example
-#   include mailcatcher
+# @param packages
+#   list of packages to be installed
+# @param ruby_version
+#   required ruby version
+# @param module_mngmt
+#   dnfmodule: manage the ruby modules
+# @param ensure_state
+#   state of the packages (installed)
+# @param package_provider
+#   provider to install ruby
+# @param terminal_provider
+#   terminal to be use: shell
+# @param http_addr
+#   custom address where the app should listen
+#   Default: 127.0.0.1
+# @param http_port
+#   custom port for the application
+#   Default: 8010
+# @param smtp_addr
+#   custom smtp address
+#   Default: 127.0.0.1
+# @param smtp_port
+#   custom smtp port
+#   Default: 2510
 class mailcatcher (
   Array[String] $packages,
   String        $ruby_version,
@@ -11,31 +34,32 @@ class mailcatcher (
   String        $ensure_state,
   String        $package_provider,
   String        $terminal_provider,
-  String        $service_desc,
-  String        $service_doc,
-  String        $service_after,
-  String        $service_type,
-  String        $service_restart,
-  String        $service_wanted,
-  String        $service_restart_time,
   Optional[Stdlib::IP::Address] $http_addr = undef,
   Optional[Stdlib::Port]        $http_port = undef,
   Optional[Stdlib::IP::Address] $smtp_addr = undef,
   Optional[Stdlib::Port]        $smtp_port = undef,
 ) {
-case $facts['os']['family'] {
-  'RedHat': {
-     if $facts['os']['release']['major'] >= '8' {
-       include mailcatcher::setruby
-       include mailcatcher::install
-       include mailcatcher::systemdunit
+  case $facts['os']['family'] {
+    'RedHat': {
+      unless $facts['os']['release']['major'] >= '7' {
+        fail("The os release (${facts['os']['family']} ${facts['os']['release']['major']}) is not supported yet.")
+      }
 
-       Class['mailcatcher::setruby']
-         -> Class['mailcatcher::install']
-         -> Class['mailcatcher::systemdunit']
-     }
-  } default: {
-      notify {"Your distro is not supported yet.": }
+      if $facts['os']['release']['major'] >= '8' {
+        include mailcatcher::setruby
+
+        Class['mailcatcher::setruby']
+        -> Class['mailcatcher::install']
+      }
+
+      include mailcatcher::install
+      include mailcatcher::systemdunit
+
+      Class['mailcatcher::install']
+      -> Class['mailcatcher::systemdunit']
+    }
+    default: {
+      fail("The distro (${facts['os']['family']}) is not supported yet.")
     }
   }
 }
